b1bdcfa98f73bf79d961ed790b2b0b9f2661a4d6
log2akshat <akshat-pg8@iiitmk.ac.in>
2015-07-26 21:12:33 +0530

    Updated

--- OAFMS/WEB-INF/src/java/net/adminsecurity/AdminSecurityService.java
+++
@@ -1,7 +0,0 @@
-package net.adminsecurity;
-import javax.security.sasl.AuthenticationException;
-
-public interface AdminSecurityService {
-	public AdminUser authenticate(String username, String password)
-			throws AuthenticationException;
-}
--- OAFMS/WEB-INF/src/java/net/security/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package net.security;
-import javax.security.sasl.AuthenticationException;
-
-public interface SecurityService {
-	public User authenticate(int oid, String password)
-			throws AuthenticationException;
-}
